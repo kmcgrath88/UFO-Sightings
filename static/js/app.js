3c180c51c231b27290cd11e053a7956053934bbf
Kristi McGrath <mcgrathkristi@gmail.com>
2020-09-08 09:56:52 -0400

    Updates

--- static/js/app.js
+++ static/js/app.js
@@ -58,52 +58,80 @@ button.on('click', runEnter);
 form.on('submit', runEnter);
 
 // Event handler function for the form
-function runEnter(ufo){ // --- ufo????
+function runEnter(){ 
 
     // Prevent page from refreshing
     d3.event.preventDefault();
 
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select('#datetime');
+    // var inputElement = d3.select(this).select('input');
+    var changedElement = d3.select(this).select('input');
+    console.log(changedElement);
+    var inputValue = changedElement.property('value');
+    var inputElement = changedElement.attr('id')
+    var new_data = {};
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property('value');
+    if (inputValue){
+        new_data[inputElement] = inputValue;
+    }
+
+    // else{
+    //     delete new_data[]
+    // }
+
+    // var inputElementDate = d3.select(this).attr('id'); //.text();
+    // // Get the value property of the input element
+    // var inputValueDate = inputElementDate.property('value');
+
+    // // Select the input element and get the raw HTML node
+    // // var inputElement = d3.select(this).select('input');
+    // var inputElementCity = d3.select('#city');
+    // // Get the value property of the input element
+    // var inputValueCity = inputElementDate.property('value');
+
+    // // var filterID = .att to pull datetime, city, etc
+
+    
+    // new_data[inputElementDate] = inputValueDate;
+    // new_data[inputElementCity] = inputValueCity;
+    console.log(new_data);
 
     console.log(inputValue);
     console.log(tableData);
 
-    var filteredData = tableData.filter(ufo_sighting => ufo_sighting.datetime === inputValue);
-    console.log(filteredData);
-
-    // Create array with ufo values ----???? way to pull all info instead of
-    // just datetime???
-    var ufo_values = filteredData.map(ufo_sighting => ufo_sighting.datetime === inputValue)
-
-    // Select the unordered list element by class name
-    var list = d3.select('#ufo-table');
-
-    // Remove any children from the list
-    //list.html('');
-
-    // Append Stats to the list
-    // list.append('tr').text(ufo_values)
-
-    // Use d3 to append one table row tr for each ufo object
-    var row = tbody.append('tr');
-
-    // Object.entries(ufo).forEach(function([key, value]){ // ----???
-    // console.log(key,value);
-        
-    // Use d3 to append 1 cell per ufo value 
-    // (datetime, city, state, country, shape, durationMinutes, comments)
-    var cell = row.append('td');
-    
-    // Use d3 to update each cell's text with ufo values
-    // (datetime, city, state, country, shape, durationMinutes, comments)
-    cell.text(ufo_values);
-
-
-    //)};
+    filter_Data();
+ 
 };
 
 
+function filter_Data(){
+// Filter by date
+Object.entries(new_data).forEach(([key,value]) => {
+    filteredData = tableData.filter(ufo_sighting => ufo_sighting[key] === value);
+});
+
+    console.log(filteredData);
+
+
+    tbody.html('')
+
+    filteredData.forEach(function(ufo){
+    console.log(ufo);
+     
+    // Use d3 to append one table row tr for each ufo object
+    var row = tbody.append('tr');
+
+    // Use object entries to console.log each ufo value
+    Object.entries(ufo).forEach(function([key, value]){
+        console.log(key,value);
+    
+    // Use d3 to append 1 cell per ufo value 
+    // (datetime, city, state, country, shape, durationMinutes, comments)
+    var cell = row.append('td');
+
+    // Use d3 to update each cell's text with ufo values
+    // (datetime, city, state, country, shape, durationMinutes, comments)
+    cell.text(value);
+    
+    });
+})};
